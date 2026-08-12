1628ac6be320c4a0b76300c1331b55de58835fa9
Brittany Mills <134399093+brittanymills593@users.noreply.github.com>
2026-08-12 14:49:37 +0100

    Update survey.py

--- survey.py
+++ survey.py
@@ -8,97 +8,162 @@ st.title("Review of morning meeting")
 
 menu = st.sidebar.selectbox("Menu", ["Fill Survey", "View Responses"])
 
+
 # ---------------- SURVEY ----------------
+
 if menu == "Fill Survey":
 
     st.write("Please answer the following questions.")
 
     # Question 1
-    st.subheader("1. What should the Monday and Friday morning meetings look like?")
+    st.subheader(
+        "1. What should the Monday and Friday morning meetings look like?"
+    )
 
     q1_1 = st.checkbox("What we do currently works well")
     q1_2 = st.checkbox("Only one meeting - Friday")
     q1_3 = st.checkbox("Only one meeting - Monday")
-    q1_4 = st.checkbox("Split into separate team meetings e.g. haemonc and solid")
-    q1_5 = st.checkbox("What we currently do but with some changes to format (suggest below)")
+    q1_4 = st.checkbox(
+        "Split into separate team meetings e.g. haemonc and solid"
+    )
+    q1_5 = st.checkbox(
+        "What we currently do but with some changes to format (suggest below)"
+    )
     q1_6 = st.checkbox("Other (write below)")
-    
+
     q1_other = st.text_area("Suggestions")
 
 
-    # Question 2
-#    st.subheader("2. What do we want included in the CS meeting?")
+    # ---------------------------------------------------------
+    # QUESTION 2 - CURRENTLY DISABLED
+    # ---------------------------------------------------------
 
-#    q2_1 = st.checkbox("The current structure is good")
-#    q2_2 = st.checkbox("The overview of solid and haemonc is repetitive from individual meetings and could be removed")
-#    q2_3 = st.checkbox("More feedback on projects going on within the department")
-#    q2_4 = st.checkbox("Presentations on interesting cases")
+    # st.subheader("2. What do we want included in the CS meeting?")
 
-#    q2_other = st.text_area("Other suggestions for Question 2")
+    # q2_1 = st.checkbox("The current structure is good")
 
-    # Question 3
-#    st.subheader("3. How could our current appraisal process be improved?")
+    # q2_2 = st.checkbox(
+    #     "The overview of solid and haemonc is repetitive from "
+    #     "individual meetings and could be removed"
+    # )
 
-#    q3_1 = st.checkbox("More guidance and structure for goals")
-#    q3_2 = st.checkbox("Mikel to attend")
-#    q3_3 = st.checkbox("Feedback from colleagues to be included")
+    # q2_3 = st.checkbox(
+    #     "More feedback on projects going on within the department"
+    # )
 
-#    q3_other = st.text_area("Other suggestions for Question 3")
+    # q2_4 = st.checkbox("Presentations on interesting cases")
 
-#    if st.button("Submit"):
+    # q2_other = st.text_area("Other suggestions for Question 2")
 
-#        response = pd.DataFrame([{
- #           "Morning Meetings": ", ".join([
-  #              option for option, selected in {
-   #                 "What we do currently works well": q1_1,
-    #                "No meeting": q1_2,
-     #               "Split into haemonc and solid": q1_3,
-      #              "What we currently do without going through the rota": q1_4
-       #         }.items() if selected
-        #    ]),
-         #   "Morning Meetings Other": q1_other,
 
-          #  "CS Meeting": ", ".join([
-           #     option for option, selected in {
-            #        "The current structure is good": q2_1,
-             #       "The overview of solid and haemonc is repetitive from individual meetings and could be removed": q2_2,
-              #      "More feedback on projects going on within the department": q2_3,
-               #     "Presentations on interesting cases": q2_4
-                #}.items() if selected
-            #]),
- #           "CS Meeting Other": q2_other,
+    # ---------------------------------------------------------
+    # QUESTION 3 - CURRENTLY DISABLED
+    # ---------------------------------------------------------
 
-#            "Appraisal Process": ", ".join([
-#                option for option, selected in {
-#                    "More guidance and structure for goals": q3_1,
- #                   "Mikel to attend": q3_2,
-  #                  "Feedback from colleagues to be included": q3_3
-   #             }.items() if selected
-    #        ]),
-     #       "Appraisal Process Other": q3_other
-     #   }])
+    # st.subheader("3. How could our current appraisal process be improved?")
+
+    # q3_1 = st.checkbox("More guidance and structure for goals")
+
+    # q3_2 = st.checkbox("Mikel to attend")
+
+    # q3_3 = st.checkbox(
+    #     "Feedback from colleagues to be included"
+    # )
+
+    # q3_other = st.text_area(
+    #     "Other suggestions for Question 3"
+    # )
+
+
+    # ---------------------------------------------------------
+    # SUBMIT
+    # ---------------------------------------------------------
+
+    if st.button("Submit"):
+
+        response = pd.DataFrame([{
+
+            "Morning Meetings": ", ".join([
+                option for option, selected in {
+                    "What we do currently works well": q1_1,
+                    "Only one meeting - Friday": q1_2,
+                    "Only one meeting - Monday": q1_3,
+                    "Split into separate team meetings e.g. haemonc and solid": q1_4,
+                    "What we currently do but with some changes to format": q1_5,
+                    "Other": q1_6
+                }.items() if selected
+            ]),
+
+            "Morning Meetings Other": q1_other
+
+            # -------------------------------------------------
+            # QUESTION 2 - DISABLED FOR NOW
+            # -------------------------------------------------
+
+            # "CS Meeting": ", ".join([
+            #     option for option, selected in {
+            #         "The current structure is good": q2_1,
+            #         "The overview of solid and haemonc is repetitive "
+            #         "from individual meetings and could be removed": q2_2,
+            #         "More feedback on projects going on within the department": q2_3,
+            #         "Presentations on interesting cases": q2_4
+            #     }.items() if selected
+            # ]),
+
+            # "CS Meeting Other": q2_other,
+
+
+            # -------------------------------------------------
+            # QUESTION 3 - DISABLED FOR NOW
+            # -------------------------------------------------
+
+            # "Appraisal Process": ", ".join([
+            #     option for option, selected in {
+            #         "More guidance and structure for goals": q3_1,
+            #         "Mikel to attend": q3_2,
+            #         "Feedback from colleagues to be included": q3_3
+            #     }.items() if selected
+            # ]),
+
+            # "Appraisal Process Other": q3_other
+
+        }])
 
         if os.path.exists(csv_file):
-            response.to_csv(csv_file, mode='a', header=False, index=False)
+            response.to_csv(
+                csv_file,
+                mode="a",
+                header=False,
+                index=False
+            )
         else:
-            response.to_csv(csv_file, index=False)
+            response.to_csv(
+                csv_file,
+                index=False
+            )
 
         st.success("Thank you! Your response has been saved.")
 
 
 # ---------------- ADMIN VIEW ----------------
+
 elif menu == "View Responses":
 
     st.subheader("Admin Access Required")
 
-    password = st.text_input("Enter password", type="password")
+    password = st.text_input(
+        "Enter password",
+        type="password"
+    )
 
     if password == "NGS":
 
         st.success("Access granted")
 
         if os.path.exists(csv_file):
+
             df = pd.read_csv(csv_file)
+
             st.dataframe(df)
 
             st.download_button(
@@ -106,6 +171,7 @@ elif menu == "View Responses":
                 df.to_csv(index=False),
                 file_name="NGS_survey_responses.csv"
             )
+
         else:
             st.warning("No responses yet.")
 
